d0787d87a4df9b6794b000b252d3def6c9c8ce3e
shahsevil <you@example.com>
2020-05-12 23:05:32 +0400

    -- structure

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -1,4 +1,61 @@
 package entities;
 
 public class User {
+
+    private int userId;
+    private String name;
+    private String surname;
+    private String userName;
+    private String password;
+    private String gender;
+    private String urlPhoto;
+
+    public User(int userId, String name, String surname, String userName, String password, String gender, String urlPhoto) {
+        this.userId = userId;
+        this.name = name;
+        this.surname = surname;
+        this.userName = userName;
+        this.password = password;
+        this.gender = gender;
+        this.urlPhoto = urlPhoto;
+    }
+
+    public User(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getUrlPhoto() {
+        return urlPhoto;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("User{userId=%d, name='%s', surname='%s', userName='%s', password='%s', gender='%s', urlPhoto='%s'}",
+                userId, name, surname, userName, password, gender, urlPhoto);
+    }
 }
