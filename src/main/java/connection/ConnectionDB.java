0820566285d72d9e6dc871ac96cd72d49402d036
Elvin <elvintaghiyev184@gmail.com>
2020-05-13 22:18:02 +0400

    "connection is working, but getConnection() method need to be implemented, commit by @elvin"

--- src/main/java/connection/ConnectionDB.java
+++ src/main/java/connection/ConnectionDB.java
@@ -4,9 +4,18 @@ import java.net.URISyntaxException;
 import java.sql.*;
 
 public class ConnectionDB {
-  //  public static Connection getConnection() throws URISyntaxException, SQLException {
-  public static void main(String[] args) throws URISyntaxException, SQLException {
-    String dbURL = System.getenv("JDBC_DATABASE_URL");
-    Connection conn = DriverManager.getConnection(dbURL, "elvintaghiyev14", "elvin123db@");
-  }
+
+    public static void main(String[] args) throws SQLException {
+//        String dbURL = System.getenv("JDBC_DATABASE_URL");
+        String dbURL = "jdbc:postgresql://ec2-54-247-78-30.eu-west-1.compute.amazonaws.com:5432/d3du1hdp316o87?user=eyscvazigqvthg&password=88a9e297385cc9f450652971aed96ba6972540ba9bf2f2b9dec40859564ae142";
+        System.out.println(dbURL);
+        ResultSet r = DriverManager.getConnection(dbURL).prepareStatement("SELECT * FROM users").executeQuery();
+        r.next();
+        System.out.println(r.getString("name"));
+
+    }
+
+    public static Connection getConnection() {
+        return null;
+    }
 }
