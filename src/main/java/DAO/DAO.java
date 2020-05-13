6ea2000f4eb05079534a8379c7e3c8ab0d366316
Elvin <elvintaghiyev184@gmail.com>
2020-05-13 16:25:26 +0400

    "initial changes by @elvintaghizade"

--- src/main/java/DAO/DAO.java
+++ src/main/java/DAO/DAO.java
@@ -1,11 +1,12 @@
 package DAO;
 
+import java.sql.SQLException;
 import java.util.Collection;
 import java.util.Optional;
 import java.util.function.Predicate;
 
 public interface DAO<A> {
-    Optional<A> get(int id);
+    Optional<A> get(int id) throws SQLException;
     Collection<A> getAllBy(Predicate<A> p);
     Collection<A> getAll();
     void update(A a);
---
+++ src/main/java/connection/DBConnection.java
@@ -0,0 +1,17 @@
+package connection;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+  public static Connection getConnection() throws URISyntaxException, SQLException {
+    URI dbUri = new URI(System.getenv("DATABASE URL"));
+    String username = dbUri.getUserInfo().split(":")[0];
+    String password = dbUri.getUserInfo().split(":")[1];
+    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+    return DriverManager.getConnection(dbUrl, username, password);
+  }
+}
