c41879b84b0530652ab5a8281c3c1b2d93935499
Elvin <elvintaghiyev184@gmail.com>
2020-05-13 19:58:05 +0400

    "initial commit - @elvin" "connection is unsuccessfull"

--- src/main/java/connection/ConnectionDB.java
+++ src/main/java/connection/ConnectionDB.java
@@ -1,13 +1,12 @@
 package connection;
 
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.sql.*;
 
 public class ConnectionDB {
   //  public static Connection getConnection() throws URISyntaxException, SQLException {
   public static void main(String[] args) throws URISyntaxException, SQLException {
-    String dbURL = System.getenv("jdbc:postgresql://ec2-54-247-78-30.eu-west-1.compute.amazonaws.com:5432/d3du1hdp316o87");
-    Connection conn = DriverManager.getConnection(dbURL);
+    String dbURL = System.getenv("JDBC_DATABASE_URL");
+    Connection conn = DriverManager.getConnection(dbURL, "elvintaghiyev14", "elvin123db@");
   }
 }
