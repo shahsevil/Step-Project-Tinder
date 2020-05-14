e647250f6f3ec98e989024a97a5857ca05d06732
Elvin <elvintaghiyev184@gmail.com>
2020-05-14 17:42:16 +0400

    "first page finished, register improved committed by @elvin"

--- src/main/java/servlets/FirstPageServlet.java
+++ src/main/java/servlets/FirstPageServlet.java
@@ -5,15 +5,31 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.stream.Collectors;
 
 public class FirstPageServlet extends HttpServlet {
 
   @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
     String result = new BufferedReader(new FileReader(new File("./src/main/resources/content/first-page.html"))).lines().collect(Collectors.joining("\n"));
     try (PrintWriter w = resp.getWriter()) {
       w.write(result);
+    }
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    String next = req.getParameter("action");
+    if (next.equals("login")) {
+      try (OutputStream os = resp.getOutputStream()) {
+        Files.copy(Paths.get("./src/main/resources/content/", "login.html"), os);
+      }
+    } else if (next.equals("register")) {
+      try (OutputStream os = resp.getOutputStream()) {
+        Files.copy(Paths.get("./src/main/resources/content/", "register.html"), os);
+      }
     }
   }
 }
--- src/main/java/servlets/RegisterServlet.java
+++ src/main/java/servlets/RegisterServlet.java
@@ -5,10 +5,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class RegisterServlet extends HttpServlet {
+
   @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    super.doGet(req, resp);
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    try (OutputStream os = resp.getOutputStream()) {
+      Files.copy(Paths.get("./src/main/resources/content/", "register.html"), os);
+    }
   }
+
+
 }
