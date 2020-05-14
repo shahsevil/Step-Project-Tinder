d725c9c6c77614d9e8c761310126348cb44f1eeb
Elvin <elvintaghiyev184@gmail.com>
2020-05-14 13:22:58 +0400

    "Connection to remote host at heroku (appName -> tinder-project) completed, committed by @elvin"

--- src/main/java/connection/ConnectionDB.java
+++ src/main/java/connection/ConnectionDB.java
@@ -2,22 +2,29 @@ package connection;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class ConnectionDB {
+  private static final String host = "ec2-54-247-78-30.eu-west-1.compute.amazonaws.com";
+  private static final int port = 5432;
+  private static final String dbname = "d3du1hdp316o87";
+  private static final String username = "eyscvazigqvthg";
+  private static final String password = "88a9e297385cc9f450652971aed96ba6972540ba9bf2f2b9dec40859564ae142";
+  private static final String JDBC_DATABASE_URL = String.format("jdbc:postgresql://%s:%d/%s?user=%s&password=%s",
+          host, port, dbname, username, password);
 
-    public static void main(String[] args) throws SQLException {
-//        String dbURL = System.getenv("JDBC_DATABASE_URL");
-        String dbURL = "jdbc:postgresql://ec2-54-247-78-30.eu-west-1.compute.amazonaws.com:5432/d3du1hdp316o87?user=eyscvazigqvthg&password=88a9e297385cc9f450652971aed96ba6972540ba9bf2f2b9dec40859564ae142";
-        System.out.println(dbURL);
-        ResultSet r = DriverManager.getConnection(dbURL).prepareStatement("SELECT * FROM users").executeQuery();
-        r.next();
-        System.out.println(r.getString("name"));
-
-    }
-
-    public static Connection getConnection() {
-        return null;
-    }
+  /**
+   * If system environmental variable configured successfully just write:
+   * String dbURL = System.getenv("JDBC_DATABASE_URL");
+   * <p>
+   * JDBC_DATABASE_URL pattern is=> jdbc:postgresql://<host>:<port>/<dbname>?user=<username>&password=<password>
+   * <p>
+   *
+   * @return Connection to database with 'JDBC_DATABASE_URL'
+   * @throws SQLException for null dbUrl
+   */
+  public static Connection getConnection() throws SQLException {
+    String dbUrl = System.getenv(JDBC_DATABASE_URL);
+    return DriverManager.getConnection(dbUrl);
+  }
 }
