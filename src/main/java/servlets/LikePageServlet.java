8a2401c1f1ac3052849498aee081c06b78c9c5c9
Elvin <elvintaghiyev184@gmail.com>
2020-05-16 22:42:38 +0400

    "LikePageService (/users) added, committed by @elvin"

--- src/main/java/servlets/LikePageServlet.java
+++ src/main/java/servlets/LikePageServlet.java
@@ -5,9 +5,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class LikePageServlet extends HttpServlet {
+  private final TemplateEngine templateEngine;
+
+  public LikePageServlet(TemplateEngine templateEngine) {
+    this.templateEngine = templateEngine;
+  }
 
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
-
+//    templateEngine.render("like-page.ftl", , resp);
   }
 }
