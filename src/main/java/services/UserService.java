ccc4517c7d097b1bb98b2caf408b99268f7de355
shahsevil <you@example.com>
2020-05-16 17:54:56 +0400

    -- TemplateEngine is working, UserServlet added

--- src/main/java/services/UserService.java
+++ src/main/java/services/UserService.java
@@ -9,16 +9,15 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class UserService {
+
     LikeDAO likeDAO;
     private UserDAO userDAO= new UserDAO();
 
-
-
     public List<User> listOfLikedUsers(int id) {
-        List<Like> likedUserIds = likeDAO.getAll().stream().filter(like -> like.getLikerUserId()==id)
+        List<Like> likes = likeDAO.getAll().stream().filter(like -> like.getLikerUserId()==id)
                 .collect(Collectors.toList());
         return userDAO.getAll().stream()
-                .filter(user -> likedUserIds.contains(new Like(id, user.getUserId(),true)))
+                .filter(user -> likes.contains(new Like(id, user.getUserId(),true)))
                 .collect(Collectors.toList());
     }
 }
