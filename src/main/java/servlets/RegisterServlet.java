346670a753adb2c0a1f47ec293f73cc50dcd6537
Elvin <elvintaghiyev184@gmail.com>
2020-05-17 21:29:01 +0400

    "RegisterServlet finished, committed by @elvin"

--- src/main/java/servlets/RegisterServlet.java
+++ src/main/java/servlets/RegisterServlet.java
@@ -3,6 +3,7 @@ package servlets;
 import entities.User;
 import services.RegisterService;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -23,12 +24,13 @@ public class RegisterServlet extends HttpServlet {
   }
 
   @Override
-  protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     String username = req.getParameter("username");
     String password = req.getParameter("password");
     String profession = req.getParameter("profession");
     String photoUrl = req.getParameter("photoUrl");
     User newUser = new User(username, password, profession, photoUrl);
     REGISTER_SERVICE.register(newUser);
+    resp.sendRedirect("/login");
   }
 }
