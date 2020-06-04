aa56323b2419d366250b57266c0a382205570818
Elvin <elvintaghiyev184@gmail.com>
2020-06-04 22:49:56 +0400

    Filter added

--- src/main/java/org/tinder_proj/filter/CookieFilter.java
+++ src/main/java/org/tinder_proj/filter/CookieFilter.java
@@ -1,15 +1,36 @@
 package org.tinder_proj.filter;
 
-import javax.servlet.FilterChain;
+import javax.servlet.*;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-public class CookieFilter implements HttpFilter {
+public class CookieFilter implements Filter {
+  private boolean isHttp(ServletRequest req) {
+    return req instanceof HttpServletRequest;
+  }
+
+  public boolean isCookieOk(HttpServletRequest req) {
+    Cookie[] cookies = req.getCookies();
+    if (cookies == null) return false;
+    throw new IllegalArgumentException("Add if statement here...");
+  }
+
   @Override
-  public void doHttpFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) {
-    for (Cookie c: request.getCookies()) {
+  public void init(FilterConfig filterConfig) {
 
-    }
+  }
+
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    if (isHttp(request) && isCookieOk((HttpServletRequest) request)) {
+      chain.doFilter(request, response);
+    } else ((HttpServletResponse) response).sendRedirect("/login");
+  }
+
+  @Override
+  public void destroy() {
+
   }
 }
